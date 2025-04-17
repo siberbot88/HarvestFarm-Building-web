14dd11d0c0ff1a18ad1fc356d12ccfb0bd985f07
Mohammad Bayu Rizki <widjanarko020@gmail.com>
2025-04-17 17:33:42 +0700

    commit

--- public/assets/index.php
+++ public/assets/index.php
@@ -38,6 +38,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HarvestFarm - Hasil Tani Berkualitas</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
